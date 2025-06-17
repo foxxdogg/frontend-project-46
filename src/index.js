6fd5751e40d70c8710db3600a83c2f9b5e4164d1
foxdog <alexkolymago@gmail.com>
2025-06-17 18:07:51 +0300

    fix: step 9.28

--- src/index.js
+++ src/index.js
@@ -1,29 +1,26 @@
 import { Command } from 'commander';
 import { loadParsedFiles, genDiff } from './buildDiff.js';
 
-const program = new Command();
-program.exitOverride();
-program
-  .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
-  .option('-f, --format [type]', 'output format', 'stylish')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
-  .action((filepath1, filepath2, options) => {
-    try {
-      const { format } = options;
-      const [file1, file2] = loadParsedFiles(filepath1, filepath2);
-      console.log(genDiff(file1, file2, format));
-    } catch (error) {
-      console.error(`Error: ${error.message}`);
-      throw error;
-    }
-  });
+const runApp = () => {
+  const program = new Command();
+  program
+    .description('Compares two configuration files and shows a difference.')
+    .version('1.0.0')
+    .option('-f, --format [type]', 'output format', 'stylish')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .action((filepath1, filepath2, options) => {
+      try {
+        const { format } = options;
+        const [file1, file2] = loadParsedFiles(filepath1, filepath2);
+        console.log(genDiff(file1, file2, format));
+      } catch (error) {
+        console.error(`Error: ${error.message}`);
+        throw error;
+      }
+    });
 
-const runApp = (argv = process.argv) => {
-  const fakeArgs = ['node', 'gendiff', 'data/file1.json', 'data/file2.json'];
-  const args = argv.length < 3 ? fakeArgs : argv;
-  program.parse(args);
+  program.parse(process.argv);
 };
 
 export default runApp;
