a1270e5a72170f3968ae2aa19efd1597a06b6468
foxdog <alexkolymago@gmail.com>
2025-06-17 14:01:15 +0300

    fix: step 9.8

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { Command } from 'commander';
 import { loadParsedFiles, genDiff } from './src/lib.js';
 
-const runApp = () => {
+const runApp = (argv = process.argv) => {
   const program = new Command();
   program
     .description('Compares two configuration files and shows a difference.')
@@ -9,19 +9,22 @@ const runApp = () => {
     .option('-f, --format [type]', 'output format', 'stylish')
     .argument('<filepath1>')
     .argument('<filepath2>')
-    .action(
-      (filepath1, filepath2, options) => {
-        try {
-          const { format } = options;
-          const [file1, file2] = loadParsedFiles(filepath1, filepath2);
-          console.log(genDiff(file1, file2, format));
-        } catch (error) {
-          console.error(`Error: ${error.message}`);
-          process.exit(1);
-        }
-      },
-    );
-  program.parse(process.argv);
+    .action((filepath1, filepath2, options) => {
+      try {
+        const { format } = options;
+        const [file1, file2] = loadParsedFiles(filepath1, filepath2);
+        console.log(genDiff(file1, file2, format));
+      } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+      }
+    });
+
+  if (argv.length <= 2) {
+    program.help({ error: true });
+  }
+
+  program.parse(argv);
 };
 
 export default runApp;
