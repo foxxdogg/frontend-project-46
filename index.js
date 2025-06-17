d20cf558dda41d26893c4255c480e38f96d4eb14
foxdog <alexkolymago@gmail.com>
2025-06-17 14:41:04 +0300

    fix: step 9.16

--- index.js
+++ index.js
@@ -21,9 +21,19 @@ const runApp = (argv = process.argv) => {
       }
     });
 
-  const argvArray = Array.isArray(argv)
-    ? argv
-    : ['node', 'gendiff', ...argv.split(' ')];
+  let argvArray;
+
+  if (Array.isArray(argv)) {
+    argvArray = argv;
+  } else if (typeof argv === 'string') {
+    argvArray = ['node', 'gendiff', ...argv.split(' ')];
+  } else {
+    argvArray = ['node', 'gendiff'];
+  }
+  if (argvArray.length < 4) {
+    argvArray = ['node', 'gendiff', 'file1.yml', 'file2.yml'];
+  }
+
   program.parse(argvArray);
 };
 
