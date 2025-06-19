b823d24cf67b5cb22950b4fb297dc8d36370f26b
foxdog <alexkolymago@gmail.com>
2025-06-19 22:01:46 +0300

    fix: step 9.32

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import { genDiff } from './buildDiff.js';
+import { loadParsedFiles, genDiff as genDiffFromData } from './buildDiff.js';
+
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
+  const [file1, file2] = loadParsedFiles(filepath1, filepath2);
+  return genDiffFromData(file1, file2, format);
+};
 
 export default genDiff;
