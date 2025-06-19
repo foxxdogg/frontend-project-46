811652d474461301ea51d1f9a7afefe852395a51
foxdog <alexkolymago@gmail.com>
2025-06-19 21:21:41 +0300

    fix: step 9.31

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,24 @@
 #!/usr/bin/env node
-import runApp from '../src/index.js';
+import { Command } from 'commander';
+import { loadParsedFiles, genDiff } from '../src/buildDiff.js';
 
-runApp();
+const program = new Command();
+program.exitOverride();
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2, options) => {
+    try {
+      const { format } = options;
+      const [file1, file2] = loadParsedFiles(filepath1, filepath2);
+      console.log(genDiff(file1, file2, format));
+    } catch (error) {
+      console.error(`Error: ${error.message}`);
+      throw error;
+    }
+  });
+
+program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,27 +1,3 @@
-import { Command } from 'commander';
-import { loadParsedFiles, genDiff } from './buildDiff.js';
+import { genDiff } from './buildDiff.js';
 
-const runApp = (argv = process.argv) => {
-  const program = new Command();
-  program.exitOverride();
-  program
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0')
-    .option('-f, --format [type]', 'output format', 'stylish')
-    .argument('<filepath1>')
-    .argument('<filepath2>')
-    .action((filepath1, filepath2, options) => {
-      try {
-        const { format } = options;
-        const [file1, file2] = loadParsedFiles(filepath1, filepath2);
-        console.log(genDiff(file1, file2, format));
-      } catch (error) {
-        console.error(`Error: ${error.message}`);
-        throw error;
-      }
-    });
-
-  program.parse(argv);
-};
-
-export default runApp;
+export default genDiff;
